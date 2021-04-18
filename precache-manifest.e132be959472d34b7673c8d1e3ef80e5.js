6f4aafea51634d0f82366b295ab488dac71577e6
Rebba Pravallika <pravallika.rebba.rao@gmail.com>
2021-04-18 17:02:00 +0530

    Updates

---
+++ precache-manifest.e132be959472d34b7673c8d1e3ef80e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44be5df54615574a44b2f2beda2626b",
+    "url": "/reactjs-covid/index.html"
+  },
+  {
+    "revision": "ab105c0c40dd2b265dc3",
+    "url": "/reactjs-covid/static/css/2.ddfbff33.chunk.css"
+  },
+  {
+    "revision": "63bca836eae98176eefa",
+    "url": "/reactjs-covid/static/css/main.0da31e43.chunk.css"
+  },
+  {
+    "revision": "ab105c0c40dd2b265dc3",
+    "url": "/reactjs-covid/static/js/2.69b39b30.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/reactjs-covid/static/js/2.69b39b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bca836eae98176eefa",
+    "url": "/reactjs-covid/static/js/main.d3c77ac7.chunk.js"
+  },
+  {
+    "revision": "55efce677ba7fdffbc0e",
+    "url": "/reactjs-covid/static/js/runtime-main.682d8200.js"
+  }
+]);
